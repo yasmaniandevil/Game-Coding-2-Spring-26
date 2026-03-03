31bfa0284bbb0f26db908d221a892deffac238a1
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2026-03-03 13:04:07 -0500

    deleted walking sim 2

--- WalkingSim2_InClass/Assets/NPCData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Dialogue/NPC Data")]
-public class NPCData : ScriptableObject
-{
-    public string displayName;
-    public string placeHolderOpeningLine;
-}
